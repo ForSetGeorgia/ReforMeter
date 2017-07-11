4bfdc1bcb06ce5ab792ec9c2926c044b465f24ae
meripepanashvili <mpepa13@freeuni.edu.ge>
2017-07-11 16:40:59 +0400

    updates

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@ set :domain, 'reformeter.jumpstart.ge'
 set :user, 'reformeter'
 set :application, 'Reformeter'
 # easier to use https; if you use ssh then you have to create key on server
-set :repository, 'https://github.com/JumpStartGeorgia/ReforMeter.git'
+set :repository, 'https://github.com/ForSetGeorgia/ReforMeter.git'
 set :branch, 'v2.0'
 set :web_url, 'reformeter.iset-pi.ge'
 set :secondary_web_url, 'reformeter.jumpstart.ge'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@ set :domain, 'alpha.jumpstart.ge'
 set :user, 'reformeter-staging'
 set :application, 'ReforMeter-Staging'
 # easier to use https; if you use ssh then you have to create key on server
-set :repository, 'https://github.com/JumpStartGeorgia/ReforMeter.git'
+set :repository, 'https://github.com/ForSetGeorgia/ReforMeter.git'
 set :branch, 'v2.0'
 set :web_url, 'dev-reformeter.jumpstart.ge'
 set :visible_to_robots, false

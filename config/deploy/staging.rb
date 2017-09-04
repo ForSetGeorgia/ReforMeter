5f04985b7b03c70afa595e146b8beea3387d8fc5
jason <jason.addie@gmail.com>
2017-09-04 12:51:54 +0400

    update deploy to use forset and ipv6

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,9 @@
-set :domain, 'alpha.jumpstart.ge'
-set :user, 'reformeter-staging'
+set :domain, '139.162.208.211' #beta.forset.ge
+set :user, 'deploy'
 set :application, 'ReforMeter-Staging'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/ForSetGeorgia/ReforMeter.git'
-set :branch, 'v2.0'
-set :web_url, 'dev-reformeter.jumpstart.ge'
+set :branch, 'master'
+set :web_url, 'dev-reformeter.forset.ge'
 set :visible_to_robots, false
-set :use_ssl, true
+set :use_ssl, false

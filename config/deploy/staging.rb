e4bbbd04ece464579efdd51b2d40db21b2caaf75
jason <jason.addie@gmail.com>
2017-09-18 15:08:33 +0400

    deploy master to staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@ set :user, 'deploy'
 set :application, 'ReforMeter-Staging'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/ForSetGeorgia/ReforMeter.git'
-set :branch, 'news'
+set :branch, 'master'
 set :web_url, 'dev-reformeter.forset.ge'
 set :visible_to_robots, false
 set :use_ssl, true

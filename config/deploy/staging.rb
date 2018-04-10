757cd909455e96ce2eb8b59da5c9500115eb2dc8
jason <jason.addie@gmail.com>
2018-04-10 12:02:56 +0400

    turn off ssl

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,9 @@
 set :domain, 'beta.forset.ge'
 set :user, 'deploy'
-set :application, 'ReforMeter-Staging'
+set :application, 'ReforMeter'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/ForSetGeorgia/ReforMeter.git'
 set :branch, 'master'
 set :web_url, 'dev-reformeter.forset.ge'
 set :visible_to_robots, false
-set :use_ssl, true
+set :use_ssl, false

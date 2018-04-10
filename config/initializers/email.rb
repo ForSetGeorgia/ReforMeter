ed05768c4c8f48dfa2d7fc850f8e78a659ea79fe
jason <jason.addie@gmail.com>
2018-04-10 12:20:20 +0400

    update email settings

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -2,12 +2,12 @@ if Rails.env.production? || Rails.env.staging?
   Rails.application.config.action_mailer.delivery_method = :smtp
 
   Rails.application.config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'forset.ge',
-    user_name: ENV['APPLICATION_FEEDBACK_FROM_EMAIL'],
-    password: ENV['APPLICATION_FEEDBACK_FROM_PWD'],
-    authentication: :plain,
-    enable_starttls_auto: true
+    :address              => ENV['APPLICATION_EMAIL_SMTP_ADDRESS'],
+    :port                 => 587,
+    :domain               => 'forset.ge',
+    :user_name            => ENV['APPLICATION_EMAIL_SMTP_AUTH_USER'],
+    :password             => ENV['APPLICATION_EMAIL_SMTP_AUTH_PASSWORD'],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
   }
 end

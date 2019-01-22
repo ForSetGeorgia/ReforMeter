07c8f12c2a7b7245a725e4529f48b6e3c5882f38
Jason <info@forset.ge>
2019-01-22 12:21:51 +0100

    pass the auth token when creating share image

--- app/assets/javascripts/application/charts/post_create_chart_images.js
+++ app/assets/javascripts/application/charts/post_create_chart_images.js
@@ -5,11 +5,14 @@ function postCreateChartImages(charts, exportType) {
 
     if (typeof pngImagePath !== 'string') return false;
 
+    var token = $('meta[name="csrf-token"]').attr('content');
+    
     $.post(
       gon.create_chart_share_image_url,
       {
         png_image_path: pngImagePath,
-        highcharts_export_options: chart.getExportOptions(exportType)
+        highcharts_export_options: chart.getExportOptions(exportType),
+        authenticity_token: token
       }
     );
 

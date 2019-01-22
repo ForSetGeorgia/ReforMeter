87cce92d7366842f5a97ab00824a5e58aa9c9842
Jason <info@forset.ge>
2019-01-22 20:40:35 +0100

    add www to allowed urls

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,6 +6,7 @@ set :repository, 'https://github.com/ForSetGeorgia/ReforMeter.git'
 set :branch, 'master'
 set :web_url, 'reformeter.iset-pi.ge'
 set :secondary_web_url, 'reformeter.forset.ge'
+set :secondary_web_url, 'www.reformeter.iset-pi.ge'
 set :use_ssl, true
 set :puma_worker_count, '1'
 set :puma_thread_count_min, '1'
